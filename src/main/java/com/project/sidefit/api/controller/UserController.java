9aba36e8b55d110127b55195a4474092fc05ffce
JunginPark4364 <wjddlsee@gmail.com>
2022-08-15 05:33:29 +0900

    Docs : Update Api Docs
    - 인증메일 재전송 API 스펙 추가
    - token 재발급 API 스펙 추가
    - 알림 관련 adoc 파일 이름 수정
    - UserController 클래스만 생성

---
+++ src/main/java/com/project/sidefit/api/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.project.sidefit.api.controller;
+
+import com.project.sidefit.api.dto.response.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserController {
+/*
+    *//**
+     * 전체 회원 목록 조회
+     *//*
+    @GetMapping("/users")
+    public Response findAll() {
+
+
+    }
+
+    *//**
+     * 회원 상세 조회
+     *//*
+    @GetMapping("/user/{id}")
+    public Response findDetail(@PathVariable Long id) {
+
+    }
+
+    *//**
+     * 비밀번호 변경
+     *//*
+    @PatchMapping("/user/password")
+    public Response updatePassword() {
+
+    }*/
+}
