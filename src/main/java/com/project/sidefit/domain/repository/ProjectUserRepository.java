4c464b955bd25bc7ce316f5369e7c1968e19a942
yunhozz <qkrdbsgh1121@naver.com>
2022-08-18 13:10:53 +0900

    Remove: Delete file

--- src/main/java/com/project/sidefit/domain/repository/ProjectUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.sidefit.domain.repository;
-
-import com.project.sidefit.domain.entity.ProjectUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectUserRepository extends JpaRepository<ProjectUser, Long> {
-}
