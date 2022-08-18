8994303dd580468f7f0f66142c9f6a33ea34183a
yunhozz <qkrdbsgh1121@naver.com>
2022-08-18 11:48:27 +0900

    Feat: Add 'findByUser' and 'findByProject'

--- src/main/java/com/project/sidefit/domain/repository/ApplyRepository.java
+++ src/main/java/com/project/sidefit/domain/repository/ApplyRepository.java
@@ -1,7 +1,14 @@
 package com.project.sidefit.domain.repository;
 
 import com.project.sidefit.domain.entity.Apply;
+import com.project.sidefit.domain.entity.Project;
+import com.project.sidefit.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ApplyRepository extends JpaRepository<Apply, Long> {
+
+    List<Apply> findByUser(User user);
+    List<Apply> findByProject(Project project);
 }
