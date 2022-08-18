b9e6a867d3edda8bd753b998f41b12f8f8b8025d
yunhozz <qkrdbsgh1121@naver.com>
2022-08-18 11:56:05 +0900

    Feat: Add service methods about finding users and ending project

--- src/main/java/com/project/sidefit/domain/service/ProjectService.java
+++ src/main/java/com/project/sidefit/domain/service/ProjectService.java
@@ -50,13 +50,63 @@ public class ProjectService {
         );
     }
 
-    public void deleteProject(Long projectId) {
+    public void endProject(Long projectId) {
+        Project project = findProject(projectId);
+        project.end();
+    }
 
+    // TODO: projectId 를 FK 로 가지고 있는 엔티티 처리
+    public void deleteProject(Long projectId) {
+        projectRepository.delete(findProject(projectId));
     }
 
     @Transactional(readOnly = true)
-    public ProjectResponseDto findProjectDto(Long teamId) {
-        return new ProjectResponseDto(findProject(teamId));
+    public List<MemberResponseDto> findProjectUserDtoListWithProjectId(Long projectId) {
+        Project project = findProject(projectId);
+        List<User> members = project.getProjectUsers().stream()
+                .map(ProjectUser::getUser)
+                .collect(Collectors.toList());
+
+        return members.stream()
+                .map(MemberResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public List<MemberResponseDto> findPreMemberDtoListWithUserId(Long userId) {
+        User user = userRepository.getReferenceById(userId);
+        List<ProjectUser> projectUserList = projectUserRepository.findByUser(user);
+        List<Long> projectIds = projectUserList.stream()
+                .map(projectUser -> projectUser.getProject().getId())
+                .collect(Collectors.toList());
+
+        return projectUserRepository.findPreMembers(projectIds);
+    }
+
+    // TODO: 점수 정렬 기준 수정
+    @Transactional(readOnly = true)
+    public List<ProjectRecommendDto> findRecommendProjectDtoListWithUserId(Long userId) {
+        User user = userRepository.getReferenceById(userId);
+        List<Project> projects = projectRepository.findAll();
+
+        // key: project, value: score
+        Map<Project, Integer> scoreMap = new HashMap<>();
+        for (Project project : projects) {
+            int score = project.recommendScoreByUser(user);
+            scoreMap.put(project, score);
+        }
+        // 각각 합산한 점수들을 내림차순으로 나열
+        List<Map.Entry<Project, Integer>> scoreMapEntry = new ArrayList<>(scoreMap.entrySet());
+        scoreMapEntry.sort(Comparator.comparingInt(Map.Entry::getValue));
+
+        return scoreMapEntry.stream()
+                .map(entry -> new ProjectRecommendDto(entry.getKey()))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public ProjectResponseDto findProjectDto(Long projectId) {
+        return new ProjectResponseDto(findProject(projectId));
     }
 
     @Transactional(readOnly = true)
@@ -84,6 +134,7 @@ public class ProjectService {
                 .stack(projectRequestDto.getStack())
                 .meetingPlan(projectRequestDto.getMeetingPlan())
                 .hashtag(projectRequestDto.getHashtag())
+                .status(true)
                 .build();
     }
 
