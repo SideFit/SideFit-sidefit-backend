04fe90e3e72b2ad113074697e9e809707c35df82
yunhozz <qkrdbsgh1121@naver.com>
2022-08-26 17:12:33 +0900

    Feat: 프로젝트 검색, 쿼리 기능 추가 (키워드, 최신순, 정확도순)

--- src/main/java/com/project/sidefit/domain/repository/project/ProjectRepositoryImpl.java
+++ src/main/java/com/project/sidefit/domain/repository/project/ProjectRepositoryImpl.java
@@ -6,8 +6,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static com.project.sidefit.api.dto.ProjectDto.*;
@@ -22,7 +21,7 @@ public class ProjectRepositoryImpl implements ProjectRepositoryCustom {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public List<ProjectQueryDto> searchProject(String keyword) {
+    public List<ProjectQueryDto> searchProjectByLatestOrder(String keyword) {
         List<ProjectQueryDto> projects = queryFactory
                 .select(new QProjectDto_ProjectQueryDto(
                         project.id,
@@ -38,6 +37,7 @@ public class ProjectRepositoryImpl implements ProjectRepositoryCustom {
                 .from(project)
                 .join(project.image, image)
                 .where(project.title.contains(keyword)
+                        .or(project.introduction.contains(keyword))
                         .or(project.hashtag.contains(keyword)))
                 .orderBy(project.createdDate.desc())
                 .fetch();
@@ -65,5 +65,77 @@ public class ProjectRepositoryImpl implements ProjectRepositoryCustom {
         projects.forEach(projectQueryDto -> projectQueryDto.setRecruits(recruitMap.get(projectQueryDto.getId())));
 
         return projects;
+    }
+
+    @Override
+    public List<ProjectQueryDto> searchProjectByAccuracyOrder(String keyword) {
+        List<ProjectQueryDto> projects = queryFactory
+                .select(new QProjectDto_ProjectQueryDto(
+                        project.id,
+                        project.title,
+                        project.type,
+                        project.hashtag,
+                        project.status,
+                        project.createdDate,
+                        project.lastModifiedDate,
+                        image.id,
+                        image.imageUrl
+                ))
+                .from(project)
+                .join(project.image, image)
+                .where(project.title.contains(keyword)
+                        .or(project.introduction.contains(keyword))
+                        .or(project.hashtag.contains(keyword)))
+                .fetch();
+
+        List<Long> projectIds = projects.stream()
+                .map(ProjectQueryDto::getId)
+                .collect(Collectors.toList());
+
+        List<RecruitResponseDto> recruits = queryFactory
+                .select(Projections.constructor(
+                        RecruitResponseDto.class,
+                        recruit.id,
+                        project.id,
+                        recruit.jobGroup,
+                        recruit.currentNumber,
+                        recruit.recruitNumber
+                ))
+                .from(recruit)
+                .join(recruit.project, project)
+                .where(project.id.in(projectIds))
+                .fetch();
+
+        Map<Long, List<RecruitResponseDto>> recruitMap = recruits.stream()
+                .collect(Collectors.groupingBy(RecruitResponseDto::getProjectId));
+        projects.forEach(projectQueryDto -> projectQueryDto.setRecruits(recruitMap.get(projectQueryDto.getId())));
+
+        Map<ProjectQueryDto, Integer> countMap = new HashMap<>();
+        for (ProjectQueryDto project : projects) {
+            int count1 = countKeyword(project.getTitle(), keyword);
+            int count2 = countKeyword(findIntroductionByProjectId(project.getId()), keyword);
+            int count3 = countKeyword(project.getHashtag(), keyword);
+            countMap.put(project, count1 + count2 + count3);
+        }
+        List<Map.Entry<ProjectQueryDto, Integer>> entries = new ArrayList<>(countMap.entrySet());
+        entries.sort((o1, o2) -> o2.getValue().compareTo(o1.getValue()));
+
+        List<ProjectQueryDto> sortedProjects = new ArrayList<>();
+        for (Map.Entry<ProjectQueryDto, Integer> entry : entries) {
+            sortedProjects.add(entry.getKey());
+        }
+        return sortedProjects;
+    }
+
+    private String findIntroductionByProjectId(Long projectId) {
+        return queryFactory
+                .select(project.introduction)
+                .from(project)
+                .where(project.id.eq(projectId))
+                .fetchOne();
+    }
+
+    private int countKeyword(String str, String keyword) {
+        return str.length() - str.replace(keyword, "").length();
     }
 }
