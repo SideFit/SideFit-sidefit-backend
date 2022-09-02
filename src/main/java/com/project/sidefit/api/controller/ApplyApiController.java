4c8339f56535295825059e2040ccbcbe173aea42
yunhozz <qkrdbsgh1121@naver.com>
2022-09-03 08:09:17 +0900

    Fix: api 관련 수정

--- src/main/java/com/project/sidefit/api/controller/ApplyApiController.java
+++ src/main/java/com/project/sidefit/api/controller/ApplyApiController.java
@@ -2,7 +2,6 @@ package com.project.sidefit.api.controller;
 
 import com.project.sidefit.api.dto.response.Response;
 import com.project.sidefit.domain.entity.User;
-import com.project.sidefit.domain.repository.ApplyRepository;
 import com.project.sidefit.domain.repository.UserJpaRepo;
 import com.project.sidefit.domain.repository.project.ProjectRepository;
 import com.project.sidefit.domain.service.ApplyService;
@@ -23,14 +22,12 @@ public class ApplyApiController {
 
     private final ApplyService applyService;
     private final ProjectService projectService;
-    private final ApplyRepository applyRepository;
     private final UserJpaRepo userRepository;
     private final ProjectRepository projectRepository;
 
     @PostMapping("/project/apply")
     public Response apply(@AuthenticationPrincipal User user, @RequestParam String projectId, @Valid @RequestBody ApplyRequestDto ApplyRequestDto) {
-        Long applyId = applyService.applyToTeam(user.getId(), Long.valueOf(projectId), ApplyRequestDto);
-        return Response.success();
+        return Response.success(applyService.applyToTeam(user.getId(), Long.valueOf(projectId), ApplyRequestDto));
     }
 
     @PostMapping("/project/invite")
@@ -39,13 +36,12 @@ public class ApplyApiController {
         if (!project.getUserId().equals(user.getId())) {
             return Response.failure(-1000, "프로젝트 관리 권한이 없습니다.");
         }
-        Long applyId = applyService.inviteToUser(Long.valueOf(receiverId), Long.valueOf(projectId), inviteRequestDto);
-        return Response.success();
+        return Response.success(applyService.inviteToUser(Long.valueOf(receiverId), Long.valueOf(projectId), inviteRequestDto));
     }
 
     // 프로젝트 팀장이 유저 지원 처리
-    @GetMapping("/project/apply-response/{applyId}")
-    public Response getApplyResponse(@AuthenticationPrincipal User user, @PathVariable String applyId, @RequestParam Boolean flag) {
+    @PostMapping("/project/apply-response")
+    public Response getApplyResponse(@AuthenticationPrincipal User user, @RequestParam String applyId, @RequestParam Boolean flag) {
         ApplyResponseDto apply = applyService.findApplyDto(Long.valueOf(applyId));
         ProjectResponseDto project = projectService.findProjectDto(apply.getProjectId());
         if (!project.getUserId().equals(user.getId())) {
@@ -55,17 +51,17 @@ public class ApplyApiController {
             return Response.failure(-1000, "유저를 찾을 수 없습니다.");
         }
         applyService.applyResponse(Long.valueOf(applyId), flag);
-        return Response.success(applyService.findApplyResultDto(Long.valueOf(applyId)));
+        return Response.success();
     }
 
     // 유저가 프로젝트 참여 제안 처리
-    @GetMapping("/project/invite-response/{applyId}")
-    public Response getInviteResponse(@AuthenticationPrincipal User user, @PathVariable String applyId, @RequestParam Boolean flag) {
+    @PostMapping("/project/invite-response")
+    public Response getInviteResponse(@AuthenticationPrincipal User user, @RequestParam String applyId, @RequestParam Boolean flag) {
         ApplyResponseDto apply = applyService.findApplyDto(Long.valueOf(applyId));
         if (projectRepository.findById(apply.getProjectId()).isEmpty()) {
             return Response.failure(-1000, "프로젝트가 존재하지 않습니다.");
         }
         applyService.inviteResponse(Long.valueOf(applyId), flag);
-        return Response.success(applyService.findApplyResultDto(Long.valueOf(applyId)));
+        return Response.success();
     }
 }
